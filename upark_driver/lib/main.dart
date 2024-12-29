891f5c67be2137d9aec0471e1743240eb38bd271
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2024-12-29 14:23:57 +0800

    misc files

--- upark_driver/lib/main.dart
+++ upark_driver/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:upark_driver/features/authentication/presentation/authentication_page.dart';
+import 'package:upark_driver/features/login/presentation/authentication_page.dart';
 
 void main() {
   runApp(const MyApp());

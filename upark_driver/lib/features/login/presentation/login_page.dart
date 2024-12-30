8338ee65b280a2a1c330c6dfdd43a31808422438
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2024-12-31 03:50:07 +0800

    login page UI done

--- upark_driver/lib/features/login/presentation/login_page.dart
+++ upark_driver/lib/features/login/presentation/login_page.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:upark_driver/core/resources/dimensions.dart';
-import 'package:upark_driver/features/login/widget/email_text_label.widget.dart';
-import 'package:upark_driver/features/login/widget/email_textfield.widget.dart';
-import 'package:upark_driver/features/login/widget/forgot_password_text_label.widget.dart';
-import 'package:upark_driver/features/login/widget/google_button.widget.dart';
-import 'package:upark_driver/features/login/widget/login_button.widget.dart';
-import 'package:upark_driver/features/login/widget/login_text_label.widget.dart';
-import 'package:upark_driver/features/login/widget/or_text_label.widget.dart';
-import 'package:upark_driver/features/login/widget/password_text_label.dart';
-import 'package:upark_driver/features/login/widget/password_textfield.widget.dart';
+import 'package:upark_driver/features/login/widget/text_label/create_acc_text_label.widget.dart';
+import 'package:upark_driver/features/login/widget/text_label/dont_have_acc_text_label.widget.dart';
+import 'package:upark_driver/features/login/widget/text_label/email_text_label.widget.dart';
+import 'package:upark_driver/features/login/widget/text_field/email_textfield.widget.dart';
+import 'package:upark_driver/features/login/widget/button/fb_button.widget.dart';
+import 'package:upark_driver/features/login/widget/text_label/forgot_password_text_label.widget.dart';
+import 'package:upark_driver/features/login/widget/button/google_button.widget.dart';
+import 'package:upark_driver/features/login/widget/button/login_button.widget.dart';
+import 'package:upark_driver/features/login/widget/text_label/login_text_label.widget.dart';
+import 'package:upark_driver/features/login/widget/text_label/or_text_label.widget.dart';
+import 'package:upark_driver/features/login/widget/text_label/password_text_label.dart';
+import 'package:upark_driver/features/login/widget/text_field/password_textfield.widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -38,23 +41,27 @@ class _LoginPageState extends State<LoginPage> {
                   height: 246,
                   width: 326,
                 ),
-                SizedBox(height: largeSpacing),
-                LoginTextLabelWidget(),
-                SizedBox(height: extraLargeSpacing),
-                EmailTextLabelWidget(),
                 SizedBox(height: smallSpacing),
+                LoginTextLabelWidget(),
+                SizedBox(height: regularSpacing),
+                EmailTextLabelWidget(),
                 EmailTextfieldWidget(),
                 SizedBox(height: smallSpacing),
                 PasswordTextLabelWidget(),
                 PasswordTextfieldWidget(),
-                SizedBox(height: extraLargeSpacing),
+                SizedBox(height: regularSpacing),
                 ForgotPasswordTextLabelWidget(),
-                SizedBox(height: extraLargeSpacing),
+                SizedBox(height: regularSpacing),
                 LoginButtonWidget(),
                 SizedBox(height: regularSpacing),
                 OrTextLabelWidget(),
                 SizedBox(height: regularSpacing),
                 GoogleButtonWidget(),
+                SizedBox(height: regularSpacing),
+                FacebookButtonWidget(),
+                SizedBox(height: regularSpacing),
+                DontHaveAccountTextLabelWidget(),
+                CreateAccountTextLabelWidget(),
               ],
             ),
           ),

aa853555d98004944f695e67c6820d686e4bbf60
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-01 19:05:53 +0800

    Misc files

--- upark_driver/lib/main.dart
+++ upark_driver/lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:upark_driver/features/login/presentation/login_page.dart';
 import 'package:upark_driver/features/splash/presentation/splash_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 //test
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SplashPage(),
+      title: 'Upark App Demo',
+      routerConfig: _router,
+
+      // home: const SplashPage(),
     );
   }
+
+  final GoRouter _router = GoRouter(
+    //this is for initial location of the app
+    initialLocation: "/login",
+    //other routes/screen for the app
+    routes: [
+      GoRoute(path: "/", builder: (context, state) => const SplashPage()),
+      GoRoute(path: "/login", builder: (context, state) => const LoginPage()),
+    ],
+  );
 }

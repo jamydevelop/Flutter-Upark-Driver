55242d278c7c3c8244403c17bb82cb010d82e1b3
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-04 07:17:32 +0800

    plate number textfield added

--- upark_driver/lib/features/register/widget/text_field/plate_number_textfield.register.widget.dart
+++ upark_driver/lib/features/register/widget/text_field/plate_number_textfield.register.widget.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:upark_driver/core/resources/colors.dart';
 import 'package:upark_driver/core/resources/dimensions.dart';
 import 'package:upark_driver/core/widget/common_text_field.dart';
 import 'package:upark_driver/features/register/widget/text_label/plate_number_text_label.register.widget.dart';
@@ -14,6 +15,20 @@ class PlateNumberTextfieldRegisterWidget extends StatelessWidget {
         SizedBox(height: extraSmallSpacing),
         CommonTextField(
           hintText: "Plate #",
+          hintTextStyle: TextStyle(
+            color: grey,
+            fontSize: 16,
+          ),
+          prefixIcon: Image.asset(
+            "assets/icons/license_icon.png",
+            scale: 3,
+          ),
+          onFieldSubmitted: (value) {
+            FocusScope.of(context).unfocus();
+          },
+          inputBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
         )
       ],
     );

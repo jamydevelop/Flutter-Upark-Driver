1b5619d7f63590a1c35190c7eb57448c01b18c2d
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-05 16:52:18 +0800

    LoginTextLabelLoginWidget fixed

--- upark_driver/lib/features/login/widget/text_label/login_text_label.login.widget.dart
+++ upark_driver/lib/features/login/widget/text_label/login_text_label.login.widget.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:upark_driver/core/resources/dimensions.dart';
+import 'package:upark_driver/core/widget/common_text_label.dart';
 
 class LoginTextLabelLoginWidget extends StatelessWidget {
   const LoginTextLabelLoginWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "Login",
-      style: TextStyle(fontSize: fontSizeTitle1, fontWeight: FontWeight.bold),
+    return CommonTextLabel(
+      text: "Login",
+      fontWeight: FontWeight.w600,
+      fontSize: 25,
     );
   }
 }

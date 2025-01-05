ef63ce6283139fc8842869951e795ac0e4d25039
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-06 01:13:12 +0800

    added inkwell on Forgot Password

--- upark_driver/lib/features/login/widget/text_label/forgot_password_text_label.login.widget.dart
+++ upark_driver/lib/features/login/widget/text_label/forgot_password_text_label.login.widget.dart
@@ -1,23 +1,38 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:upark_driver/core/resources/colors.dart';
 import 'package:upark_driver/core/resources/dimensions.dart';
 import 'package:upark_driver/core/widget/common_text_label.dart';
 
-class ForgotPasswordTextLabelLoginWidget extends StatelessWidget {
+class ForgotPasswordTextLabelLoginWidget extends StatefulWidget {
   const ForgotPasswordTextLabelLoginWidget({super.key});
+
+  @override
+  ForgotPasswordTextLabelLoginWidgetState createState() =>
+      ForgotPasswordTextLabelLoginWidgetState();
+}
+
+class ForgotPasswordTextLabelLoginWidgetState
+    extends State<ForgotPasswordTextLabelLoginWidget> {
+  // This variable holds the color that changes when tapped
+  Color textColor = greenPrimary; // Initially, it is greenPrimary
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        GestureDetector(
-          onTap: () {},
+        InkWell(
+          onTap: () {
+            setState(() {
+              // Change the color to red when tapped
+              textColor = greyPrimary;
+            });
+          },
           child: CommonTextLabel(
             fontFamily: "Roboto",
             fontWeight: FontWeight.w600,
             fontSize: fontSizeCaption1, //12px
-            color: greenPrimary, //#379237
+            color: textColor, // Use the dynamic color based on the state
             text: "Forgot Password",
           ),
         ),

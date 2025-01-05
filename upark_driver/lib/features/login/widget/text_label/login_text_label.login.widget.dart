e06aed0a05da778a1a2dd5615fabacbfe14a3db8
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-06 00:47:03 +0800

    change the color of the Login Text Label on Login

--- upark_driver/lib/features/login/widget/text_label/login_text_label.login.widget.dart
+++ upark_driver/lib/features/login/widget/text_label/login_text_label.login.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:upark_driver/core/resources/colors.dart';
 import 'package:upark_driver/core/resources/dimensions.dart';
 import 'package:upark_driver/core/widget/common_text_label.dart';
 
@@ -12,6 +13,7 @@ class LoginTextLabelLoginWidget extends StatelessWidget {
       fontWeight: FontWeight.w600,
       fontSize: fontSizeTitle1, //25px
       text: "Login",
+      color: blackPrimary,
     );
   }
 }

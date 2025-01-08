a34e9056a174476bc713a7a3f97eba8d6a961b4f
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-08 22:12:44 +0800

    added initial folder for onboarding

--- upark_driver/lib/core/resources/app_router.dart
+++ upark_driver/lib/core/resources/app_router.dart
@@ -1,18 +1,21 @@
 import 'package:go_router/go_router.dart';
 import 'package:upark_driver/features/login/presentation/login_page.dart';
+import 'package:upark_driver/features/onboarding/presentation/onboarding_page.dart';
 import 'package:upark_driver/features/register/presentation/register_page.dart';
 import 'package:upark_driver/features/splash/presentation/splash_page.dart';
 
 class AppRouter {
   final GoRouter router = GoRouter(
     //this is for initial location of the app
-    initialLocation: "/register",
+    initialLocation: "/onboarding",
     //other routes/screen for the app
     routes: [
       GoRoute(path: "/splash", builder: (context, state) => const SplashPage()),
       GoRoute(path: "/login", builder: (context, state) => const LoginPage()),
       GoRoute(
           path: "/register", builder: (context, state) => const RegisterPage()),
+      GoRoute(
+          path: "/onboarding", builder: (context, state) => OnboardingPage()),
     ],
   );
 }

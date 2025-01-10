1bb7cc648ffea6b5b02133c6e366fd0ea9b7e835
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-10 23:46:11 +0800

    added route to forgot password

--- upark_driver/lib/features/login/widget/text_label/forgot_password_text_label.login.widget.dart
+++ upark_driver/lib/features/login/widget/text_label/forgot_password_text_label.login.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:upark_driver/core/resources/colors.dart';
 import 'package:upark_driver/core/resources/dimensions.dart';
 import 'package:upark_driver/core/widget/common_text_label.dart';
@@ -26,6 +27,7 @@ class ForgotPasswordTextLabelLoginWidgetState
             setState(() {
               // Change the color to red when tapped
               textColor = greyPrimary;
+              context.go("/forgotpassword");
             });
           },
           child: CommonTextLabel(

b8406ec8e9a44a9f6bdd5858435e61150005d535
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-11 11:05:27 +0800

    update bottom navigation bar

--- upark_driver/lib/features/bottom_nav/presentation/bottom_nav.page.dart
+++ upark_driver/lib/features/bottom_nav/presentation/bottom_nav.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:upark_driver/core/resources/colors.dart';
 
 class BottomNavPage extends StatefulWidget {
   const BottomNavPage({super.key});
@@ -9,26 +10,51 @@ class BottomNavPage extends StatefulWidget {
 
 class _BottomNavPageState extends State<BottomNavPage> {
   int currentPageIndex = 0;
+
+  void _onTappedItem(int index) {
+    setState(() {
+      currentPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: <Widget>[
-        Text("First Page"),
-        Text("Second Page"),
-        Text("Last Page"),
+        Center(child: Text("First Page")), // Ensure the content is centered
+        Center(child: Text("Second Page")),
+        Center(child: Text("Last Page")),
       ][currentPageIndex],
-      bottomNavigationBar: NavigationBar(
-        destinations: <Widget>[
-          NavigationDestination(icon: Icon(Icons.home_rounded), label: "label"),
-          NavigationDestination(icon: Icon(Icons.home_rounded), label: "label"),
-          NavigationDestination(icon: Icon(Icons.home_rounded), label: "label")
-        ],
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
+      bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: _buildIcon("assets/icons/home_icon.png", 0),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon("assets/icons/bookings_icon.png", 1),
+              label: "Bookings",
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon("assets/icons/profile_icon.png", 2),
+              label: "Profile",
+            ),
+          ],
+          currentIndex: currentPageIndex,
+          onTap: _onTappedItem,
+          selectedItemColor: blackPrimary),
+    );
+  }
+
+  Widget _buildIcon(String assetPath, int index) {
+    return ColorFiltered(
+      colorFilter: ColorFilter.mode(
+        currentPageIndex == index ? blackPrimary : greySecondary,
+        BlendMode.srcIn,
+      ),
+      child: Image.asset(
+        assetPath,
+        scale: 3,
       ),
     );
   }

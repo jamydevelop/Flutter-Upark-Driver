d29e7b138d037f29a0083e3eaedce7a7ab64124b
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-22 19:29:03 +0800

    Available parking widget done

--- upark_driver/lib/features/home/widget/available_parking_space.widget.dart
+++ upark_driver/lib/features/home/widget/available_parking_space.widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:upark_driver/core/resources/colors.dart';
+import 'package:upark_driver/core/resources/dimensions.dart';
+import 'package:upark_driver/core/widget/common_text_label.dart';
 
 class AvailableParkingSpaceWidget extends StatelessWidget {
   const AvailableParkingSpaceWidget({super.key});
@@ -18,6 +20,7 @@ class AvailableParkingSpaceWidget extends StatelessWidget {
         ),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
             children: [
@@ -29,14 +32,61 @@ class AvailableParkingSpaceWidget extends StatelessWidget {
                     topLeft: Radius.circular(8),
                     topRight: Radius.circular(8),
                   ),
-                  color: Colors.pink,
                 ),
                 child: Image.asset(
                   "assets/images/parking_image.png",
                   fit: BoxFit.fill,
                 ),
               ),
+              Positioned(
+                bottom: 8.0,
+                left: 90.0,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 35,
+                  height: extraNormalSpacing,
+                  decoration: BoxDecoration(
+                    color: whitePrimary,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: CommonTextLabel(
+                    text: "200 m",
+                    fontWeight: FontWeight.w400,
+                    fontSize: 7,
+                  ),
+                ),
+              ),
             ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: extraSmallSpacing, top: extraSmallSpacing),
+            child: Column(
+              spacing: extraSmallSpacing,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CommonTextLabel(
+                    text: "SM City Davao",
+                    fontWeight: FontWeight.w500,
+                    fontSize: fontSizeTitle6,
+                    color: blackPrimary),
+                Row(
+                  children: [
+                    Image.asset(
+                      "assets/icons/p_icon.png",
+                      width: extraNormalSpacing,
+                      height: extraNormalSpacing,
+                    ),
+                    SizedBox(width: extraSmallSpacing),
+                    CommonTextLabel(
+                        text: "8 Slots Available",
+                        fontWeight: FontWeight.w400,
+                        fontSize: fontSizeCaption1,
+                        color: blackPrimary),
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),

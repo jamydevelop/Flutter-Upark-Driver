5d67deb168ae1281c935ebf2768a973c5a18c21e
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-27 20:36:42 +0800

    misc files

--- upark_driver/lib/features/homepage/bookings/widget/to_pay_card.widget.dart
+++ upark_driver/lib/features/homepage/bookings/widget/to_pay_card.widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:upark_driver/core/resources/colors.dart';
 import 'package:upark_driver/core/resources/dimensions.dart';
-import 'package:upark_driver/core/widget/common_button.dart';
 import 'package:upark_driver/core/widget/common_text_label.dart';
 
 class ToPayCardWidget extends StatelessWidget {

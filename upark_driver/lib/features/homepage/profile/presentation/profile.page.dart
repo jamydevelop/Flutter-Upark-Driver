1f63e9599cdbb0f0b6c51af0efa346ec4c4ad184
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-28 14:31:31 +0800

    added image on the center of the profile page

--- upark_driver/lib/features/homepage/profile/presentation/profile.page.dart
+++ upark_driver/lib/features/homepage/profile/presentation/profile.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:upark_driver/core/resources/dimensions.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,6 +11,30 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: superLargeSpacing),
+            SizedBox(height: superLargeSpacing),
+            Container(
+              width: 80,
+              height: 74,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                shape: BoxShape.circle,
+              ),
+              child: ClipOval(
+                child: Image.asset(
+                  "assets/images/edit_profile_img.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

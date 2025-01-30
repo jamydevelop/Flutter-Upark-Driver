093f0ee61e1169f5a1117734d4929dc78d466be0
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-30 21:20:53 +0800

    added all profile icons

--- upark_driver/lib/core/resources/assets.dart
+++ upark_driver/lib/core/resources/assets.dart
@@ -5,6 +5,15 @@ class Assets {
   static const profileIcon = "$filePath/profile_icon.png";
   static const bellIcon = "$filePath/bell_icon.png";
   static const messageIcon = "$filePath/message_icon.png";
-  static const profileFavorite = "$filePath/profile_favorites.png";
+
   static const profileArrow = "$filePath/profile_arrow.png";
+  static const profileFavorite = "$filePath/profile_favorites.png";
+  static const profileBookings = "$filePath/profile_calendar.png";
+  static const profileNotifications = "$filePath/profile_notification.png";
+  static const profileChangePassword = "$filePath/profile_key.png";
+  static const profileVehicleDetails = "$filePath/profile_car.png";
+  static const profileTermsAndPolicies = "$filePath/profile_info_circle.png";
+  static const profileHelpAndSupport = "$filePath/profile_sms_edit.png";
+  static const profileDeleteAccount = "$filePath/profile_delete.png";
+  static const profileLogout = "$filePath/profile_login.png";
 }
---
+++ upark_driver/lib/features/homepage/profile/widget/bookings_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookingsWidget extends StatelessWidget {
+  const BookingsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

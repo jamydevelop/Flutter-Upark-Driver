6ac32bac12b89d9cca444f09499bf327d6739e4b
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-01-31 13:06:42 +0800

    misc files

--- upark_driver/lib/core/resources/assets.dart
+++ upark_driver/lib/core/resources/assets.dart
@@ -1,19 +1,23 @@
 class Assets {
-  static const filePath = "assets/icons";
-  static const homeIcon = "$filePath/home_icon.png";
-  static const bookingsIcon = "$filePath/bookings_icon.png";
-  static const profileIcon = "$filePath/profile_icon.png";
-  static const bellIcon = "$filePath/bell_icon.png";
-  static const messageIcon = "$filePath/message_icon.png";
+  static const filePath1 = "assets/icons";
+  static const filePath2 = "assets/images";
 
-  static const profileArrow = "$filePath/profile_arrow.png";
-  static const profileFavorite = "$filePath/profile_favorites.png";
-  static const profileBookings = "$filePath/profile_calendar.png";
-  static const profileNotifications = "$filePath/profile_notification.png";
-  static const profileChangePassword = "$filePath/profile_key.png";
-  static const profileVehicleDetails = "$filePath/profile_car.png";
-  static const profileTermsAndPolicies = "$filePath/profile_info_circle.png";
-  static const profileHelpAndSupport = "$filePath/profile_sms_edit.png";
-  static const profileDeleteAccount = "$filePath/profile_delete.png";
-  static const profileLogout = "$filePath/profile_login.png";
+  static const homeIcon = "$filePath1/home_icon.png";
+  static const bookingsIcon = "$filePath1/bookings_icon.png";
+  static const profileIcon = "$filePath1/profile_icon.png";
+  static const bellIcon = "$filePath1/bell_icon.png";
+  static const messageIcon = "$filePath1/message_icon.png";
+
+  static const editProfileImage = "$filePath2/edit_profile_img.png";
+
+  static const profileArrow = "$filePath1/profile_arrow.png";
+  static const profileFavorite = "$filePath1/profile_favorites.png";
+  static const profileBookings = "$filePath1/profile_calendar.png";
+  static const profileNotifications = "$filePath1/profile_notification.png";
+  static const profileChangePassword = "$filePath1/profile_key.png";
+  static const profileVehicleDetails = "$filePath1/profile_car.png";
+  static const profileTermsAndPolicies = "$filePath1/profile_info_circle.png";
+  static const profileHelpAndSupport = "$filePath1/profile_sms_edit.png";
+  static const profileDeleteAccount = "$filePath1/profile_delete.png";
+  static const profileLogout = "$filePath1/profile_login.png";
 }
--- upark_driver/lib/features/homepage/edit_profile/presentation/edit_profile.page.dart
+++ upark_driver/lib/features/homepage/edit_profile/presentation/edit_profile.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:upark_driver/core/resources/assets.dart';
 import 'package:upark_driver/core/widget/common_back_appbar.dart';
 
 class EditProfilePage extends StatefulWidget {
@@ -16,6 +17,11 @@ class _EditProfilePageState extends State<EditProfilePage> {
       appBar: CommonBackAppbar(
         context: context,
         onTap: () => context.go("/login"),
+      ),
+      body: Column(
+        children: [
+          Image.asset(Assets.editProfileImage),
+        ],
       ),
     );
   }

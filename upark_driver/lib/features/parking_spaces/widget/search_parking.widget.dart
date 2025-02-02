bef2c22a5a2f406978e6d846d6c265742f77ab7f
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-02-02 13:52:42 +0800

    search bar for parking spaces done

--- upark_driver/lib/features/parking_spaces/widget/search_parking.widget.dart
+++ upark_driver/lib/features/parking_spaces/widget/search_parking.widget.dart
@@ -9,26 +9,40 @@ class SearchParkingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CommonTextField(
-      hintText: "Email",
-      hintTextStyle: TextStyle(
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0), // Add border radius here
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.2), // Shadow color
+            offset: Offset(0, 1), // x=0, y=1
+            blurRadius: 7.1, // Blur effect
+            spreadRadius: 0, // No spread
+          ),
+        ],
+      ),
+      child: CommonTextField(
+        hintText: "Search Parking Spaces",
+        hintTextStyle: TextStyle(
           fontFamily: "Roboto",
           fontWeight: FontWeight.w400,
-          fontSize: fontSizeSubhead, //15px
-          color: greySecondary), //#868686
-      prefixIcon: Image.asset(
-        Assets.parkingSpacesSearchIcon,
-        scale: 2,
-      ),
-
-      onFieldSubmitted: (value) {
-        // Hide the on-screen keyboard
-        FocusScope.of(context).unfocus();
-      },
-      keyboardType: TextInputType.emailAddress,
-      inputBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16.0),
-        borderSide: BorderSide(color: greySecondary, width: 1),
+          fontSize: fontSizeTitle2,
+          color: blackPrimary,
+        ),
+        prefixIcon: Image.asset(
+          Assets.parkingSpacesSearchIcon,
+          scale: 2,
+        ),
+        onFieldSubmitted: (value) {
+          // Hide the on-screen keyboard
+          FocusScope.of(context).unfocus();
+        },
+        keyboardType: TextInputType.emailAddress,
+        inputBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
       ),
     );
   }

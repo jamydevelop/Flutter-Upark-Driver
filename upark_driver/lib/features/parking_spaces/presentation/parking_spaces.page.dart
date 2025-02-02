e6c320f448c946cc397ceec417e92f3580d5b9de
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-02-02 13:20:11 +0800

    misc files

--- upark_driver/lib/features/parking_spaces/presentation/parking_spaces.page.dart
+++ upark_driver/lib/features/parking_spaces/presentation/parking_spaces.page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:upark_driver/core/widget/common_back_appbar.dart';
 
 class ParkingSpacesPage extends StatefulWidget {
   const ParkingSpacesPage({super.key});
@@ -10,6 +12,12 @@ class ParkingSpacesPage extends StatefulWidget {
 class _ParkingSpacesPageState extends State<ParkingSpacesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: CommonBackAppbar(
+        context: context,
+        onTap: () => context.go("/home"),
+        textTitle: "Parking Spaces",
+      ),
+    );
   }
 }

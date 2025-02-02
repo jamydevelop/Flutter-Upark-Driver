e116ec3180197dcb57e99a05ddcb229dfa522792
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-02-02 13:32:57 +0800

    added initial syntax on parking spaces page

--- upark_driver/lib/features/parking_spaces/presentation/parking_spaces.page.dart
+++ upark_driver/lib/features/parking_spaces/presentation/parking_spaces.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:upark_driver/core/resources/dimensions.dart';
 import 'package:upark_driver/core/widget/common_back_appbar.dart';
 
 class ParkingSpacesPage extends StatefulWidget {
@@ -17,6 +18,16 @@ class _ParkingSpacesPageState extends State<ParkingSpacesPage> {
         context: context,
         onTap: () => context.go("/bottomnav"),
         textTitle: "Parking Spaces",
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: largeSpacing, vertical: extraLargeSpacing),
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
       ),
     );
   }

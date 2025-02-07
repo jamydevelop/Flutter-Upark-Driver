63e13a79d9b54e27056d592eeb607eeee0430744
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-02-07 14:28:07 +0800

    back to notification creation

--- upark_driver/lib/core/resources/app_router.dart
+++ upark_driver/lib/core/resources/app_router.dart
@@ -13,7 +13,7 @@ import 'package:upark_driver/features/splash/presentation/splash_page.dart';
 class AppRouter {
   final GoRouter router = GoRouter(
     //this is for initial location of the app
-    initialLocation: "/bottomnav/0",
+    initialLocation: "/notifications",
     //other routes/screen for the app
     routes: [
       GoRoute(path: "/splash", builder: (context, state) => const SplashPage()),
